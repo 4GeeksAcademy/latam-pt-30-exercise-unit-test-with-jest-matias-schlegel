2f836b24627b2c91234c1b63ac2c5acf65f209f9
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-04-29 20:42:07 +0000

    finalizado

--- test.js
+++ test.js
@@ -8,15 +8,15 @@ test('adds 14 + 9 to equal 23', function() {
 
 test("One euro should be 1.07 dollars", function() {
 const dollars = fromEuroToDollar(3.5);
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(dollars).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
 test('converts 10 dollars to yen', function() {
     let amountInYen = fromDollarToYen(10);
-    expect(fromDollarToYen(10)).toBe(1462.6168224299065);
+    expect(amountInYen).toBe(1462.6168224299065);
 });
 
 test('converts 500 yen to pound', function() {
     let amountInPound = fromYenToPound(500);
-    expect(fromYenToPound(500)).toBe(2.7795527156549524); 
+    expect(amountInPound).toBe(2.7795527156549524); 
 });
